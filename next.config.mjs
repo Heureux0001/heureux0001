4b2f2fc1448bb4d4d5b2d4376420f7c00a2770ca
Heureux <cpmarmolbusiness@gmail.com>
2024-12-01 01:24:32 +0800

    tes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/Heureux0001',
+  assetPrefix: '/Heureux0001',
 };
 
 export default nextConfig;
